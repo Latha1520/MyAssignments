a8ef5e89d5f066ac4e546f325c0e8dc0c60220fa
Latha1520 <latha152070@gmail.com>
2025-07-19 17:30:01 +0530

    Add files via upload
    Day 1 Assignment

---
+++ Chrome.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float Version = 91.0f;
+		
+		System.out.println("Version:"+" "+Version);
+		
+		String developer = "Google";
+		
+		System.out.println("Developer: "+developer);
+		
+		boolean Isbeta = false; 
+		
+		System.out.println("Isbeta: "+Isbeta);
+		
+		int releaseYear = 2008;
+		
+		System.out.println("ReleaseYear: "+releaseYear);
+		
+		char shortcutKey = 'C';
+		
+		System.out.println("ShortcutKey: "+shortcutKey);
+
+	}
+
+}
